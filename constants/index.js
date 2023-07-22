dfc1925b4fcda75bd87af2a3a0de20b114e4fe6b
Faryad Mohamadi <95682633+Faryad700@users.noreply.github.com>
2023-07-23 01:54:25 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x94e2000fc77ee18eed0c4893d3f0bd96f37b53bf";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
